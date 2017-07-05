5c6ff885d913780f2ecd47252a1c136e48784f39
Administrator <Administrator@USER-20161011FN>
2017-07-05 10:33:29 +0800

    全局配置axios

--- vue_php_wx/src/main.js
+++ vue_php_wx/src/main.js
@@ -5,12 +5,13 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: {
+		App
+	}
 })
