8ac0975cff7523b146f783a163e17b1870d226f6
Administrator <Administrator@USER-20161011FN>
2017-07-06 23:25:33 +0800

    axios 问题

--- vue_php_wx/src/actions/connect_sql.php
+++ vue_php_wx/src/actions/connect_sql.php
@@ -1,7 +1,11 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With,'application/json;charset=utf-8',Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
+//会请求两次的问题
+if (strtoupper($_SERVER['REQUEST_METHOD']) == 'OPTIONS') {
+	exit ;
+}
 //	创建连接
 $localhost = 'localhost';
 $root = 'root';
--- vue_php_wx/src/actions/register.php
+++ vue_php_wx/src/actions/register.php
@@ -3,13 +3,15 @@
 //	获取客户端传来数据
 $user = $_POST["user"];
 $psw = $_POST["password"];
-echo "帐号为" . $user . "<br>" . "密码为" . $psw . "<br>";
+var_dump($user);
+echo "帐号为" . $user . "密码为" . $psw;
 
-$insertdb = "INSERT INTO user (id, password) VALUES ('{$user}','{$psw}')";
-
-if ($conn -> query($insertdb) === TRUE) {
-	echo "新记录插入成功";
-} else {
+if (!empty($user)) {
+	$insertdb = "INSERT INTO user (id, password) VALUES ('{$user}','{$psw}')";
+	if ($conn -> query($insertdb) === TRUE) {
+		echo "新记录插入成功";
+	} else {
 		echo "Error: " . $sql . "<br>" . $conn -> error;
+	}
 }
 ?>`
