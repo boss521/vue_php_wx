986455206f7c0e92e90484278701e5415f896acb
Administrator <Administrator@USER-20161011FN>
2017-07-09 09:19:56 +0800

    小试

--- vue_php_wx/src/actions/send_character.php
+++ vue_php_wx/src/actions/send_character.php
@@ -1,9 +1,10 @@
 <?php
 @include ('connect_sql.php');
 @$cont = $_GET['contents'];
-//echo $cont;
 date_default_timezone_set('PRC');
+//获取时间
 $date = date('Y/m/d H:i:s');
+//插入数据库
 $insertContents = "INSERT INTO Content (send_id,contents,send_time) VALUES ('liming','{$cont}','{$date}')";
 if ($conn -> query($insertContents) === TRUE) {
 	echo "插入内容成功啦";
