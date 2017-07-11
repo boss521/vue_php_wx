89293875c575a2cb686e3dab06d8ecd40fee6566
Administrator <Administrator@USER-20161011FN>
2017-07-12 04:18:52 +0800

    self——actives

--- vue_php_wx/src/actions/get_friend_actives.php
+++ vue_php_wx/src/actions/get_friend_actives.php
@@ -1,7 +1,7 @@
 <?php
 @include ('connect_sql.php');
 $user=$_GET['user'];
-$sql = "SELECT c.send_id,c.contents,c.send_time,u.name,u.header FROM `content` c LEFT JOIN user u ON c.send_id = u.id WHERE c.send_id in (select  friendsid from friend where userid = '{$user}') ORDER BY c.send_time desc";
+$sql = "SELECT c.send_id,c.contents,c.send_time,u.name,u.header FROM `content` c LEFT JOIN user u ON c.send_id = u.id WHERE c.send_id in (select  friendsid from friend where userid = '{$user}' or c.send_id = '{$user}') ORDER BY c.send_time desc";
 $result = $conn -> query($sql);
 if ($result -> num_rows > 0) {
 	while ($row = $result -> fetch_array()) {
--- vue_php_wx/src/components/friends.vue
+++ vue_php_wx/src/components/friends.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="friends">
 		<div class="wall">
-			<span>{{self_info.name}}</span>
+			<span>{{self_info.user}}</span>
 			<div class="your_head">
 				<img :src="self_info.self_head" alt="">
 			</div>
@@ -27,8 +27,7 @@
 			return {
 				self_info: {
 					self_head: require('../assets/images/touxiang.jpg'),
-					user: '',
-					name:''
+					user: ''
 					
 				},
 				actives: [{}]
@@ -43,7 +42,6 @@
 			this.self_info.user = this.$cookie.get('user');
 			this.$http.get('http://192.168.1.95/dashboard/moniweixin/vue_php_wx/src/actions/get_friend_actives.php?user=' + that.self_info.user).then((response) => {
 				var get_data = response.data;
-				console.log(get_data)
 				that.actives = get_data;
 			}, (response) => {
 				alert("获取数据失败");
