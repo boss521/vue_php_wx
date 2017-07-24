3f681c16648c00c49a5f241888d1f24ad2df4e25
Administrator <Administrator@USER-20161011FN>
2017-07-24 19:35:26 +0800

    没有好友时遍历错误问题

--- vue_php_wx/src/actions/get_address_list.php
+++ vue_php_wx/src/actions/get_address_list.php
@@ -1,14 +1,17 @@
 <?php
 @include ('connect_sql.php');
-$cook=$_GET['cook'];
+$cook = $_GET['cook'];
 //echo "$cook";
-$sql= "select user.name,user.header from friend left join user on friend.friendsid=user.userid where friend.userid = '{$cook}'";
+$sql = "select user.name,user.header from friend left join user on friend.friendsid=user.userid where friend.userid = '{$cook}'";
 $result = $conn -> query($sql);
 if ($result -> num_rows > 0) {
 	while ($row = $result -> fetch_array()) {
 		$give_data[] = $row;
 	}
+	echo json_encode($give_data);
+} else {
+	echo 0;
 }
-echo json_encode($give_data);
+
 $conn -> close();
 ?>
