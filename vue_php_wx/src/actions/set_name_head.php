ff6705334d5a80ad66a8174f7b81b6f3bd8b39a2
Administrator <Administrator@USER-20161011FN>
2017-07-25 14:56:27 +0800

    解决头像上传问题

--- vue_php_wx/src/actions/set_name_head.php
+++ vue_php_wx/src/actions/set_name_head.php
@@ -3,10 +3,14 @@
 //	获取客户端传来数据
 $user = $_POST["user"];
 $psw = $_POST["password"];
+
+//$user=this.$cookie.get('user');
+//$psw=this.$cookie.get('psw');
 $name = $_POST["name"];
+$head = $_POST["head"];
 echo $user . $psw . $name;
 if (!empty($name)) {
-	$insertdb = "UPDATE user set name='{$name}' WHERE userid='{$user}' and password='{$psw}'";
+	$insertdb = "UPDATE user set name='{$name}',header='{$head}' WHERE userid='{$user}' and password='{$psw}'";
 	if ($conn -> query($insertdb) === TRUE) {
 		echo "name插入成功";
 	} else {
